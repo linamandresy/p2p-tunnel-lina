ff0fea88bcee30ad218d71aca84032ffc8b73702
Lina Mandresy <mandresyrab@gmail.com>
2024-10-13 16:21:40 +0300

    Communication TCP

--- client/main.go
+++ client/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"client/config"
 	"fmt"
-	"log"
+	"net"
 	"os"
 )
 
@@ -11,8 +11,41 @@ func main() {
 
 	config, err := config.InitConfig()
 	if err != nil {
-		log.Fatalln(os.Stderr, err)
+		panic(err)
 	}
-	fmt.Printf("Serveur : %s\n", config.Server.Host)
-	fmt.Printf("Port : %d\n", config.Server.Port)
+	serverAddress := fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port)
+	conn, err := net.Dial("tcp", serverAddress)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	go receivePackets(conn)
+
+	for {
+		fmt.Println("Enter a message to send to other client : ")
+		var msg string
+		fmt.Scanln(&msg)
+
+		fmt.Println("Enter the IP adress of the destination : ")
+		var destIp string
+		fmt.Scanln(&destIp)
+
+		packet := fmt.Sprintf("%s:%s", destIp, msg)
+
+		conn.Write([]byte(packet))
+	}
+}
+
+func receivePackets(conn net.Conn) {
+	buf := make([]byte, 4096)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Connection closed by server")
+			os.Exit(1)
+		}
+		packet := buf[:n]
+		fmt.Printf("%s\n", packet)
+	}
 }
---
+++ server/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"sync"
+)
+
+var Clients = make(map[string]net.Conn)
+var mu sync.Mutex
+
+func main() {
+	listenner, err := net.Listen("tcp", ":1309")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Server started on port:1309")
+	for {
+		conn, err := listenner.Accept()
+		if err != nil {
+			continue
+		}
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	ip := assignIP(conn)
+	fmt.Printf("New Client connected with IP : %s\n", ip)
+
+	mu.Lock()
+	Clients[ip] = conn
+	mu.Unlock()
+
+	go forwarPacket(conn, ip)
+}
+
+func forwarPacket(conn net.Conn, ip string) {
+	defer conn.Close()
+	buf := make([]byte, 4096)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+			return
+		}
+		packet := buf[:n]
+		destIp := extractIP(packet)
+		mu.Lock()
+		destConn, ok := Clients[destIp]
+		if ok {
+			destConn.Write(packet)
+		}
+		mu.Unlock()
+	}
+}
+
+func extractIP(packet []byte) string {
+	return strings.Split(string(packet), ":")[0]
+}
+
+func assignIP(conn net.Conn) string {
+	ip := fmt.Sprintf("10.0.0.%d", len(Clients)+1)
+	conn.Write([]byte(fmt.Sprintf("Your are connected as %s", ip)))
+	return ip
+}
